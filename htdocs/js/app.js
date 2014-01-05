eda11dfca738247f525ecafb07c92ec821be5851
Roman Himmes <himmes@idev.de>
2014-01-05 17:13:04 +0100

    Websocket experiments (2)

--- htdocs/js/app.js
+++ htdocs/js/app.js
@@ -1,47 +1,116 @@
+
+var RMIImpl = function() {
+    this.wsOpen = false;
+    this.ws = new WebSocket("ws://localhost:8889");
+    var that = this;
+
+    this.clearCurrentRequest = function() {
+        this.successCallback = null;
+        this.failCallback = null;
+        this.cmd = '';
+    };
+
+    this.ws.onopen = function() {
+        console.log("Socket Open!");
+        that.wsOpen = true;
+        that.clearCurrentRequest();
+    };
+
+    this.ws.onerror = function() {
+        if (that.failCallback) failCallback();
+        that.clearCurrentRequest();
+    };
+
+    this.ws.onclose = function() {
+        if (that.failCallback) failCallback();
+        that.clearCurrentRequest();
+    };
+
+    this.ws.onmessage = function(evt) {
+        var reply = JSON.parse(evt.data);
+        if (reply['cmd'] === that.cmd) {
+            if (that.successCallback) that.successCallback(reply['reply']);
+        } else {
+            if (that.failCallback) that.failCallback();
+        }
+        that.clearCurrentRequest();
+    };
+
+    this.send = function(cmd, data, successCallback, failCallback) {
+        this.successCallback = successCallback;
+        this.failCallback = failCallback;
+        this.cmd = cmd;
+        var payload = {
+            "cmd": cmd,
+            "params": data
+        };
+
+        this.ws.send(JSON.stringify(payload));
+    };
+  };
+
+var RMI = new RMIImpl();
+
 App = Ember.Application.create();
 
-App.IndexController = Ember.Controller.extend({
-
-  loginFailed: false,
-  isProcessing: false,
-  isSlowConnection: false,
-  timeout: null,
-
-  actions: {
-	  login: function() {
-	    this.setProperties({
-	      loginFailed: false,
-	      isProcessing: true
-	    });
-
-	    this.set("timeout", setTimeout(this.slowConnection.bind(this), 250));
-
-	    var request = $.post("/login", this.getProperties("username", "password"));
-	    request.then(this.success.bind(this), this.failure.bind(this));
-	  },
-  },
-
-  success: function() {
-    console.log('---------', arguments);
-    this.reset();
-    // sign in logic
-  },
-
-  failure: function() {
-    this.reset();
-    this.set("loginFailed", true);
-  },
-
-  slowConnection: function() {
-    this.set("isSlowConnection", true);
-  },
-
-  reset: function() {
-    clearTimeout(this.get("timeout"));
-    this.setProperties({
-      isProcessing: false,
-      isSlowConnection: false
-    });
-  }
-
+App.Router.map(function() {
+    this.resource('inbox');
 });
+
+App.ApplicationController = Ember.Controller.extend({
+    isLoggedIn: false,
+
+    loginFailed: false,
+    isProcessing: false,
+    isSlowConnection: false,
+    timeout: null,
+
+    actions: {
+        login: function() {
+            this.setProperties({
+              loginFailed: false,
+              isProcessing: true
+            });
+
+            this.set("timeout", setTimeout(this.slowConnection.bind(this), 250));
+
+            RMI.send("login", this.getProperties("username", "password"), this.success.bind(this), this.failure.bind(this));
+        }
+    },
+
+    success: function(reply) {
+        this.reset();
+        if (reply === 1) {
+            this.set("isLoggedIn", true);
+        } else {
+            this.set("loginFailed", true);
+        }
+    },
+
+    failure: function() {
+        this.reset();
+        this.set("loginFailed", true);
+    },
+
+    slowConnection: function() {
+        this.set("isSlowConnection", true);
+    },
+
+    reset: function() {
+        clearTimeout(this.get("timeout"));
+        this.setProperties({
+            isProcessing: false,
+            isSlowConnection: false
+        });
+    }
+});
+
+
+App.InboxRoute = Ember.Route.extend({
+    model: function() {
+        console.log("Returning IndexModel");
+        return {};
+        RMI.send("openInbox",
+    }
+});
+
--- server.js
+++ server.js
@@ -1,15 +1,19 @@
-var http = require("http"),
-    url = require("url"),
-    path = require("path"),
-    fs = require("fs")
-    port = process.argv[2] || 8888;
+var http = require("http");
+var url = require("url");
+var path = require("path");
+var fs = require("fs");
+var ws = require("ws");
 
+var Imap = require("imap");
+var inspect = require('util').inspect;
+
+var port = process.argv[2] || 8888;
 
 function handleLogin(request, response) {
   request.addListener("data", function(chunk) {
     request.content += chunk;
   });
-   
+
     request.addListener("end", function() {
       console.log('Data: ', request.content);
 
@@ -36,18 +40,18 @@ String.prototype.endsWith = function(suffix) {
 };
 
 function getContentType(filename) {
-	if (filename.endsWith('.html') || filename.endsWith('.htm') ) {
-		return 'text/html';
-	} else if (filename.endsWith('.js')) {
-		return 'application/javascript';
-	} else if (filename.endsWith('.css')) {
-		return 'text/css';
-	}
-	return 'text/plain';
+    if (filename.endsWith('.html') || filename.endsWith('.htm') ) {
+        return 'text/html';
+    } else if (filename.endsWith('.js')) {
+        return 'application/javascript';
+    } else if (filename.endsWith('.css')) {
+        return 'text/css';
+    }
+    return 'text/plain';
 }
- 
+
 http.createServer(function(request, response) {
-  request.setEncoding("utf8");
+  request.setEncoding('utf8');
   request.content = '';
 
   var uri = url.parse(request.url).pathname
@@ -62,20 +66,111 @@ http.createServer(function(request, response) {
     console.log('Requesting filename: ', filename);
 
     if (fs.statSync(filename).isDirectory()) filename += '/index.html';
- 
+
     fs.readFile(filename, "binary", function(err, file) {
-      if(err) {        
+      if(err) {
         response.writeHead(500, {"Content-Type": "text/plain"});
         response.write(err + "\n");
         response.end();
         return;
       }
- 
+
       response.writeHead(200, {"Content-Type": getContentType(filename)});
       response.write(file, "binary");
       response.end();
     });
   });
 }).listen(parseInt(port, 10));
- 
+
 console.log("Static file server running at\n  => http://localhost:" + port + "/\nCTRL + C to shutdown");
+
+// WEBSOCKET -----------------------------------------------
+
+var WebSocketServer = require('ws').Server;
+var wss = new WebSocketServer({port: port+1});
+
+wss.on('connection', function(ws) {
+    ws.on('message', function(message) {
+        console.log('received: ', message);
+        var parsedMsg = JSON.parse(message);
+        if (parsedMsg['cmd'] == 'login') {
+            var data = parsedMsg['params'];
+            var loggedIn = false;
+            if (data['username'] === 'roman' && data['password'] == '4321') {
+                loggedIn = true;
+            }
+            ws.send(JSON.stringify({"cmd": "login", "reply": loggedIn ? 1 : 0}));
+            return;
+        }
+
+        ws.send(JSON.stringify({"error": 1, "description": "Illegal command"}));
+    });
+});
+
+
+// IMAP -----------------------------------------------
+
+
+var cachedEmails = [];
+
+
+var imap = new Imap({
+    user: 'XXX',
+    password: 'XXXX',
+    host: 'XXX',
+    port: 993,
+    tls: true,
+    tlsOptions: { rejectUnauthorized: false }
+});
+
+function openInbox(cb) {
+  imap.openBox('INBOX', true, cb);
+}
+
+imap.once('ready', function() {
+    openInbox(function(err, box) {
+        if (err) throw err;
+        var f = imap.seq.fetch('1:30', {
+            bodies: 'HEADER.FIELDS (FROM TO SUBJECT DATE)',
+            struct: false
+        });
+        f.on('message', function(msg, seqno) {
+            console.log('Message #%d', seqno);
+            var prefix = '(#' + seqno + ') ';
+            msg.on('body', function(stream, info) {
+                var buffer = '';
+                stream.on('data', function(chunk) {
+                    buffer += chunk.toString('utf8');
+                });
+                stream.once('end', function() {
+                    var parsedHeader = inspect(Imap.parseHeader(buffer));
+                    cachedEmails[seqno] = parsedHeader;
+                    console.log(prefix + 'Parsed header: %s', parsedHeader);
+                });
+            });
+            msg.once('attributes', function(attrs) {
+                console.log(prefix + 'Attributes: %s', inspect(attrs, false, 8));
+            });
+            msg.once('end', function() {
+                console.log(prefix + 'Finished');
+            });
+        });
+        f.once('error', function(err) {
+            console.log('Fetch error: ' + err);
+        });
+        f.once('end', function() {
+            console.log('Done fetching all messages!');
+            imap.end();
+        });
+    });
+});
+
+imap.once('error', function(err) {
+    console.log(err);
+});
+
+imap.once('end', function() {
+    console.log('Connection ended');
+});
+
+imap.connect();
